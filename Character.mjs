acf6b1c65960f162f3132de41028050e0419f8c9
CARLOS <carlos012100@gmail.com>
2025-04-09 08:44:45 +0200

    [CREATE]json files, copied data, create classes

---
+++ Character.mjs
@@ -0,0 +1,12 @@
+export default class Character {
+
+    constructor(name, occupation, gold, life, weapon, pouch){
+
+        this.name = name; 
+        this.occupation = occupation; //job name, ex; assasin, rogue;
+        this.gold = gold;
+        this.life = life;
+        this.weapon = weapon;
+        this.pouch = pouch;
+    }
+}
